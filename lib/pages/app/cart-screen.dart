d2040773cc6859df560bebad05617e30ce5f56f8
JoseRG03 <114616625+JoseRG03@users.noreply.github.com>
2025-01-25 15:37:09 -0400

    Cart Page Mock Complete

--- lib/pages/app/cart-screen.dart
+++ lib/pages/app/cart-screen.dart
@@ -8,10 +8,51 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       color: lightGrey,
-      child: Center(
-        child: Text('Cart'),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text('My Cart', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+            SizedBox(height: 30,),
+            CartItemCard()
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class CartItemCard extends StatelessWidget {
+  const CartItemCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.horizontal(left: Radius.circular(25)),
+            child: Image(image: AssetImage('assets/images/sneakers-1.png'),
+            width: 75, height: 75, fit: BoxFit.cover,),
+          ),
+          SizedBox(width: 15,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Zoom FREAK', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                Text('\$236'),
+              ],
+            ),
+          ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.delete))
+        ],
+      ),
+      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(25)),
     );
   }
 }
