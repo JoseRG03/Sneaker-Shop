bae8029dccb1a15e23383bc5bf251dffb0b1e9e3
JoseRG03 <114616625+JoseRG03@users.noreply.github.com>
2025-01-24 20:06:57 -0400

    Initial Setup

---
+++ lib/pages/landing-page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Column(
+        children: [
+          Image(image: AssetImage('asset')),
+          
+        ],
+      ),),
+    );
+  }
+}
